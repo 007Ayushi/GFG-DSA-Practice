d9edc99fc08432e58276ef2188b3836b895794d3
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-03-20 20:17:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Size of Binary Tree/size-of-binary-tree.java
+++ Difficulty: Basic/Size of Binary Tree/size-of-binary-tree.java
@@ -20,9 +20,9 @@ class Solution {
     public static int getSize(Node node) {
         // code here
         if(node==null)return 0;
-        int c=1;
-        int a=getSize(node.left);
-        int b=getSize(node.right);
+        int a=1;//for the root node
+        int b=getSize(node.left);
+        int c=getSize(node.right);
         return a+b+c;
     }
 }
