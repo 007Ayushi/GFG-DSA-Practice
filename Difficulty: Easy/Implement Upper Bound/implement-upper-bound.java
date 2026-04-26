f4a457cd8332cab59ee8c16647f4825eb1c5aa68
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-04-26 22:09:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Implement Upper Bound/implement-upper-bound.java
+++ Difficulty: Easy/Implement Upper Bound/implement-upper-bound.java
@@ -2,21 +2,21 @@ class Solution {
     int upperBound(int[] arr, int target) {
         // code here
         
+        
         int lo=0,hi=arr.length-1;
-        
-        int res=arr.length;
-        
+        int ub=-1;
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
             
             if(arr[mid]>target){
-                res=mid;
+                ub=mid;
                 hi=mid-1;
             }
             else{
                 lo=mid+1;
             }
         }
-        return res;
+        if(ub==-1)return arr.length;
+        return ub;
     }
 }
