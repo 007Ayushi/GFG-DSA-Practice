4b1064d92b25b54f413d4c8db3db56411c1bd153
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-04-26 23:52:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.java
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.java
@@ -2,13 +2,54 @@ class Solution {
     int countFreq(int[] arr, int target) {
         // code here
         
-        //linear search 
-        int cnt=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==target){
-                cnt++;
+        //linear search -o(n)
+        // int cnt=0;
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]==target){
+        //         cnt++;
+        //     }
+        // }
+        // return cnt;
+        
+        
+        //Lower Bound
+        int lo=0,hi=arr.length-1;
+        int lb=-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            
+            if(arr[mid]==target){
+             lb=mid;
+             hi=mid-1;
+            }
+            else if(arr[mid]>target){
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
             }
         }
-        return cnt;
+        
+        //upper bound
+        lo=0;hi=arr.length-1;
+        int ub=-1;
+        
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            
+            if(arr[mid]==target){
+                ub=mid;
+                lo=mid+1;
+            }
+            else if(arr[mid]>target){
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        
+        if(lb==-1)return 0;
+        return ub-lb+1;
     }
 }
