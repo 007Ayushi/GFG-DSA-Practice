f2d179a06f01101b04d60900164255d91bac7938
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-05-24 17:58:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Power Of Numbers/power-of-numbers.java
+++ Difficulty: Medium/Power Of Numbers/power-of-numbers.java
@@ -1,24 +1,24 @@
 class Solution {
     public int reverseExponentiation(int n) {
         // code here
-        
-        int a=n;
-        int b=reverse(a);
-        
-        return pow(a,b);
+       
+      int r=rev(n);
+      return pow(n,r);
+      
     }
+    
     public int pow(int a,int b){
         if(b==0)return 1;
         return a*pow(a,b-1);
     }
-    public int reverse(int n){
-        int rev=0;
-        
+    public static int rev(int n){
+        int r=0;
         while(n!=0){
             int rem=n%10;
-            rev=rev*10+rem;
-            n=n/10;
+            r=r*10+rem;
+            n=n/10;  
         }
-        return rev;
+        return r;
     }
+    
 }
