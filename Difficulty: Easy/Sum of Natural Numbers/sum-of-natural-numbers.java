21ebe1118daea2ada87be19d22bac4d8121c8efb
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-06-05 14:39:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sum of Natural Numbers/sum-of-natural-numbers.java
+++ Difficulty: Easy/Sum of Natural Numbers/sum-of-natural-numbers.java
@@ -1,12 +1,23 @@
-// User function Template for Java
+import java.util.Scanner;
 
-class Solution {
-    static int sumOfNaturals(int n) {
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
         // code here
-        int ans=0;
-       for(int i=1;i<=n;i++){
-           ans+=i;
-       }
-       return ans;
+        
+        int s=0;
+        sum(s,n);
+        
+        
     }
-};
+    
+    public static void sum(int s,int n){
+        if(n==0){
+            System.out.print(s);
+            return;
+        }
+        sum(s+n,n-1);
+    }
+}
