ef77b51d8dae5a73009b0de2741d62cd28739ad5
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-06-07 10:22:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count ways with 3 moves/count-ways-with-3-moves.java
+++ Difficulty: Easy/Count ways with 3 moves/count-ways-with-3-moves.java
@@ -3,8 +3,8 @@ class Solution {
     static int countWays(int n) {
         // add your code here
         
-        if(n==1 || n==2)return n;
         if(n==3)return 4;
+        if(n<=2)return n;
         return countWays(n-1)+countWays(n-2)+countWays(n-3);
     }
 }
